9e9fbafc50055fcc12e445ce28c577d06cc904a0
Fabien Dagnat <fabien.dagnat@imt-atlantique.fr>
2020-11-24 14:59:28 +0100

    adding some magic to util and a todo

--- util.ml
+++ util.ml
@@ -1,1 +1,5 @@
-let rewind lexbuf n = lexbuf.Lexing.lex_curr_pos <- lexbuf.Lexing.lex_curr_pos - n
+let rewind lexbuf n =
+  (* TODO we should ensure that n is not too large *)
+  let open Lexing in
+  lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_cnum = lexbuf.lex_curr_p.pos_cnum - n; } ;
+  lexbuf.lex_curr_pos <- lexbuf.lex_curr_pos - n

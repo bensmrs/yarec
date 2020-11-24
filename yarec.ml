a67bcb2496815f85e26d818256e1a0edf042d7c1
Fabien Dagnat <fabien.dagnat@imt-atlantique.fr>
2020-11-24 14:58:05 +0100

    adding a first dune file

--- yarec.ml
+++ yarec.ml
@@ -1,4 +1,6 @@
+open Yarec
+
 let _ =
     let lexbuf = Lexing.from_channel stdin in
-    let res = Parser.start (Lexer.token []) lexbuf in
+    let _res = Parser.start (Lexer.token []) lexbuf in
     print_endline "RegEx parsed"
